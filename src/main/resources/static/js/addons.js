4bfb698d7efdc30fc97da817b4f8f33d3c018334
nayj3l <nigelarugay@gmail.com>
2025-09-24 02:12:17 +0800

    update addons

--- src/main/resources/static/js/addons.js
+++ src/main/resources/static/js/addons.js
@@ -1,3 +1,22 @@
+function getSelectedVariants() {
+    const selectedVariants = [];
+    const quantityInputs = document.querySelectorAll('#variantQuantities input[type="number"]');
+    
+    quantityInputs.forEach(input => {
+        const quantity = parseInt(input.value);
+        if (quantity > 0) {
+            selectedVariants.push({
+                variantId: input.getAttribute('data-variant-id'),
+                variantName: input.getAttribute('data-variant-name'),
+                price: parseFloat(input.getAttribute('data-price')),
+                quantity: quantity
+            });
+        }
+    });
+    
+    return selectedVariants;
+}
+
 function updateSelectedAddons() {
     // store selected addons temporarily for the item being added
     const selectedAddons = [];
@@ -12,51 +31,68 @@ function updateSelectedAddons() {
     return selectedAddons;
 }
 
-function addVariantWithAddons() {
-    // if (!selectedVariant) {
-    //     alert("Please select a variant!");
-    //     return;
-    // }
-
-    const quantity = 1;
+// Main function to add variants to order
+function addVariantsWithAddons() {
+    const selectedVariants = getSelectedVariants();
     const selectedAddons = updateSelectedAddons();
-
-    const existingIndex = currentOrder.items.findIndex(
-        i => i.itemId === selectedVariant.variantId
-    );
-
-    if (existingIndex >= 0) {
-        currentOrder.items[existingIndex].quantity += quantity;
-
-        // merge addons
-        selectedAddons.forEach(sa => {
-            const existingAddon = currentOrder.items[existingIndex].addons.find(a => a.addonId === sa.addonId);
-            if (existingAddon) existingAddon.quantity += sa.quantity;
-            else currentOrder.items[existingIndex].addons.push(sa);
-        });
-    } else {
-        currentOrder.items.push({
-            itemId: selectedVariant.variantId,
-            itemName: `${selectedItemName} (${selectedVariant.variantName})`,
-            itemPrice: selectedVariant.price,
-            quantity,
-            addons: selectedAddons
-        });
+    
+    // Check if at least one variant has quantity > 0
+    if (selectedVariants.length === 0) {
+        alert("Please select at least one variant with quantity greater than 0!");
+        return;
     }
-
+    
+    // Add each variant to the order
+    selectedVariants.forEach(variant => {
+        // Check if this variant already exists in the order
+        const existingIndex = currentOrder.items.findIndex(
+            item => item.itemId === variant.variantId
+        );
+        
+        if (existingIndex >= 0) {
+            // Update existing variant quantity
+            currentOrder.items[existingIndex].quantity += variant.quantity;
+            
+            // Merge addons
+            selectedAddons.forEach(sa => {
+                const existingAddon = currentOrder.items[existingIndex].addons.find(a => a.addonId === sa.addonId);
+                if (existingAddon) {
+                    existingAddon.quantity += sa.quantity;
+                } else {
+                    currentOrder.items[existingIndex].addons.push({...sa});
+                }
+            });
+        } else {
+            // Add new variant with addons
+            currentOrder.items.push({
+                itemId: variant.variantId,
+                itemName: `${selectedItemName} (${variant.variantName})`,
+                itemPrice: variant.price,
+                quantity: variant.quantity,
+                addons: selectedAddons.map(addon => ({...addon})) // Clone addons
+            });
+        }
+    });
+    
     calculateTotal();
     renderModalOrder();
-
-    // auto-close modal
+    
     bootstrap.Modal.getInstance(document.getElementById("variantModal")).hide();
-     
-    // Reset selections
-    selectedVariant = null;
-    selectedItemName = "";
+    resetSelections();
+}
 
+function resetSelections() {
+    // Reset quantity inputs
+    document.querySelectorAll('#variantQuantities input[type="number"]').forEach(input => {
+        input.value = 0;
+    });
+    
     // Reset addon checkboxes
-    document.querySelectorAll("#addonOptions input[type=checkbox]").forEach(cb => cb.checked = false);
-    updateSelectedAddons(); // clear selectedAddons array if you store it globally
+    document.querySelectorAll("#addonOptions input[type=checkbox]").forEach(cb => {
+        cb.checked = false;
+    });
+    
+    currentItemName = "";
 }
 
 document.querySelectorAll("#addonOptions > div").forEach(card => {
