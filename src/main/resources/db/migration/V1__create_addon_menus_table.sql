ae3d065d6f40d56090396792e91f494b915ba8c7
nayj3l <nigelarugay@gmail.com>
2025-09-28 04:59:21 +0800

    add menu image

--- src/main/resources/db/migration/V1__create_addon_menus_table.sql
+++ src/main/resources/db/migration/V1__create_addon_menus_table.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS addon_menus (
   CONSTRAINT fk_addon_menus_addon FOREIGN KEY (addon_id) REFERENCES addons(addon_id),
   CONSTRAINT fk_addon_menus_menu  FOREIGN KEY (menu_id)  REFERENCES menu(id)
 ) ENGINE=InnoDB;
+
+ALTER TABLE menu ADD COLUMN image_url VARCHAR(500);
+
+ALTER TABLE menu_items ADD COLUMN image_url VARCHAR(500);
+
+ALTER TABLE addons ADD COLUMN image_thumbnail_url VARCHAR(500);
